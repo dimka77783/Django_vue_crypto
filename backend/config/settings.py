39eee5b2a195fc474d7a102ad22a5274346cdf20
odinokov <dimka77783@mail.ru>
2025-08-04 19:16:21 +0300

    init

---
+++ backend/config/settings.py
@@ -0,0 +1,23 @@
+# Celery
+import os
+
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://redis:6379/0')
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
+
+# Django REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
+}
+
+# CORS
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+]
